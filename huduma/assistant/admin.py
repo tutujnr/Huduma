8897afc9631fe5f706440b1ec5a180e03815cfac
Langat Evans Cheruiyot. <107247196+tutujnr@users.noreply.github.com>
2026-05-13 19:52:56 +0300

    Update admin.py

--- huduma/assistant/admin.py
+++ huduma/assistant/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(TaskStep)
+admin.site.register(TaskMessage)
+admin.site.register(StatusHistory)
